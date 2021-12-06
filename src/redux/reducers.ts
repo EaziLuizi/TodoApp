65cc9683af9444f5fdbbad79f6762c6c73d22ddd
Marco Luizinho <marcoluizinho@gmail.com>
2021-12-06 17:50:36 +0200

    Adds local storage to persist todo list in the browser

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,9 +1,20 @@
 import { ADD_TODO, TOGGLE_COMPLETED, DELETE_TODO } from "./actionTypes";
 import { TodoItem } from "../Interfaces/shared";
 
-const initialState: TodoItem[] = [
-  //{ id: 1, description: "Change the World!", completed: false },
-];
+const getLocalStorageData = (): TodoItem[] => {
+  var stored = JSON.parse(localStorage.getItem("todo-list") ?? "[]");
+  if(stored.length > 0){
+    return stored;
+  }
+  return [];
+}
+
+const updateLocalStorage = (newState) => {
+  localStorage.setItem("todo-list", JSON.stringify(newState));
+  console.log(newState);
+}
+
+const initialState: TodoItem[] = getLocalStorageData();
 
 const todos = (state = initialState, action: any) => {
   switch (action.type) {
@@ -14,6 +25,7 @@ const todos = (state = initialState, action: any) => {
         description: action.payload.description,
         completed: false,
       });
+      updateLocalStorage(newState);
       return newState;
     }
     case TOGGLE_COMPLETED: {
@@ -22,10 +34,12 @@ const todos = (state = initialState, action: any) => {
       if (item) {
         item.completed = !item.completed;
       }
+      updateLocalStorage(newState);
       return newState;
     }
     case DELETE_TODO: {
       let newState = state.filter(x => x.id !== action.payload.id);
+      updateLocalStorage(newState);
       return newState;
     }
     default: {
